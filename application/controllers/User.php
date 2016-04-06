0027f658f5b24f8dd67b3a19ccce492a0885ef04
mashfiqcse13 <mashfiqnahid@gmail.com>
2016-04-07 02:28:25 +0630

    Initializing login panel

---
+++ application/controllers/User.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author MD. Mashfiq
+ */
+class User extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+
+        $this->load->helper(array('form', 'url'));
+        $this->load->library('form_validation');
+        $this->load->helper('security');
+        $this->load->library('tank_auth');
+        $this->lang->load('tank_auth');
+
+        if (!$this->tank_auth->is_logged_in()) {         // logged in
+            redirect('auth');
+        }
+    }
+    
+    function index(){
+    }
+
+}
